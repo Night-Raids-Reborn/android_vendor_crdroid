d3d47149feefb84fa134917bff6bd1afef3d7231
Advaith Bhat <advaithbhat9@gmail.com>
2021-04-26 08:57:48 +0000

    vendor: Introduce Custom Statusbar Signal & Wi-Fi icons
    - Thanks to sis, acherus, iconicicons & cool_modules_69 for these beautiful icons.
    
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Signed-off-by: darknius20 <darknius21@gmail.com>
    
    xdroidUI: Add 5 more Custom Statusbar Signal & Wi-Fi icons [2/2]
    
    - Thanks to sis, acherus, iconicicons & cool_modules_69 for these beautiful icons.
    
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    
    xdroidUI: import more signal & wifi icon
    
    Signed-off-by: darknius20 <darknius21@gmail.com>
    Signed-off-by: darknius09 <darkniuslightness@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -228,5 +228,61 @@ ifneq ($(PRODUCT_DEFAULT_DEV_CERTIFICATE),build/target/product/security/testkey)
 endif
 endif
 
+# Statusbar Icons
+PRODUCT_PACKAGES += \
+    AcherusSignalOverlay \
+    DoraSignalOverlay \
+    StrokeSignalOverlay \
+    SneakySignalOverlay \
+    XperiaSignalOverlay \
+    ZigZagSignalOverlay \
+    WavySignalOverlay \
+    RoundSignalOverlay \
+    InsideSignalOverlay \
+    BarsSignalOverlay \
+    AquariumSignalOverlay \
+    ButterflySignalOverlay \
+    DaunSignalOverlay \
+    DecSignalOverlay \
+    DeepSignalOverlay \
+    EqualSignalOverlay \
+    FanSignalOverlay \
+    HuaweiSignalOverlay \
+    RelSignalOverlay \
+    ScrollSignalOverlay \
+    SeaSignalOverlay \
+    StackSignalOverlay \
+    WannuiSignalOverlay \
+    WindowsSignalOverlay \
+    WingSignalOverlay \
+    CircleSignalOverlay \
+    IosSignalOverlay \
+    MiniSignalOverlay \
+    OdinSignalOverlay \
+    PillsSignalOverlay \
+    RomanSignalOverlay  \
+    GradiconSignalOverlay \
+    NothingDotSignalOverlay \
+    OutlineSignalOverlay \
+    PUISignalOverlay 
+	
+# Wi-Fi Icons
+PRODUCT_PACKAGES += \
+    DoraWiFiOverlay \
+    AcherusWiFiOverlay \
+    StrokeWiFiOverlay \
+    SneakyWiFiOverlay \
+    XperiaWiFiOverlay \
+    ZigZagWiFiOverlay \
+    WavyWiFiOverlay \
+    WeedWiFiOverlay \
+    RoundWiFiOverlay \
+    InsideWiFiOverlay \
+    BarsWiFiOverlay \
+    GradiconWiFiOverlay \
+    OutlineWiFiOverlay \
+    NothingDotWiFiOverlay \
+    PavlovaWiFiOverlay
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
